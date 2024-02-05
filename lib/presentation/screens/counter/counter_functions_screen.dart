ef0e0d2604cc4ee0e67a451de95a3b33b3e0b7fe
DrowLink <pauloriveiro01@gmail.com>
2024-02-05 13:49:05 -0400

    counter function view enhanced

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -19,25 +19,48 @@ class _CounterFunctionScreenState extends State<CounterFunctionScreen> {
         title: const Text('Counter Functions'),
         leading: IconButton(
           icon: const Icon( Icons.refresh_rounded ), onPressed: () { 
-
+            setState(() {
+              clickCounter = 0;
+            });
          },)
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //  Center(
+            //   child: Text(clickCounter >= 10 ? 'Llegaste a 10 taps' : '', style: const TextStyle( fontSize: 50), textAlign: TextAlign.center,)
+            //   ),
             Text(clickCounter.toString(), style: const TextStyle( fontSize: 160, fontWeight: FontWeight.w100 ),),
-             Text('Click${ clickCounter <= 1 ? '':'s'}', style: const TextStyle( fontSize: 25),)
+            Text('Click${ clickCounter <= 1 ? '':'s'}', style: const TextStyle( fontSize: 25),)
           ],
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { 
-          clickCounter++;
-          setState(() {});
-         },
-        child: const Icon( Icons.plus_one ),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () { 
+      //     clickCounter++;
+      //     setState(() {});
+      //    },
+      //   child: const Icon( Icons.plus_one ),
+      // ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(onPressed: () {
+              clickCounter++;
+              setState(() {});
+              },
+              child: const Icon(Icons.plus_one),
+            ),
+
+            FloatingActionButton(onPressed: () {
+                clickCounter--;
+                setState(() {});
+              },
+              child: const Icon(Icons.exposure_minus_1_outlined),
+            ),  
+          ]
+          ),
     );
   }
 }
